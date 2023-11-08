ef080d95ed9cc047af1214808b0ca068f85869eb
rigann <rigannurtdinov@gmail.com>
2023-11-08 19:50:39 +0300

    Added delete, editState functions

--- app/src/main/java/com/example/tobuyapp/MainViewModel.kt
+++ app/src/main/java/com/example/tobuyapp/MainViewModel.kt
@@ -1,11 +1,11 @@
 package com.example.tobuyapp
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.data.ShopListRepositoryImpl
 import com.example.domain.entity.ShopItem
 import com.example.domain.usecases.DeleteShopItemUseCase
+import com.example.domain.usecases.EditShopItemUseCase
 import com.example.domain.usecases.GetShopListUseCase
 
 class MainViewModel : ViewModel() {
@@ -13,13 +13,24 @@ class MainViewModel : ViewModel() {
     private val repository = ShopListRepositoryImpl
 
     private val getShopListUseCase = GetShopListUseCase(repository)
-    private val deleteShopListUseCase = DeleteShopItemUseCase(repository)
-    private val editShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
 
     val shopList = MutableLiveData<List<ShopItem>>()
 
     fun getShopList() {
         val list = getShopListUseCase.getShopList()
         shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem = shopItem)
+        getShopList()
+    }
+
+    fun changeEnableShopItemState(shopItem: ShopItem) {
+        val newShopItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(shopItem = newShopItem)
+        getShopList()
     }
 }
