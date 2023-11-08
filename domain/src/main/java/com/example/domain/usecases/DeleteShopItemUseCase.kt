f584ba161fec488898bca85126a4a4985d8c9936
rigann <rigannurtdinov@gmail.com>
2023-11-08 18:24:06 +0300

    Fix domain ShopListRepository and DeleteShopItemUseCase

--- domain/src/main/java/com/example/domain/usecases/DeleteShopItemUseCase.kt
+++ domain/src/main/java/com/example/domain/usecases/DeleteShopItemUseCase.kt
@@ -1,10 +1,11 @@
 package com.example.domain.usecases
 
+import com.example.domain.entity.ShopItem
 import com.example.domain.repository.ShopListRepository
 
 class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
-    fun deleteShopItem(id: Int) {
-        shopListRepository.deleteShopItem(id = id)
+    fun deleteShopItem(shopItem: ShopItem) {
+        shopListRepository.deleteShopItem(shopItem = shopItem)
     }
 }
